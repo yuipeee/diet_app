8af9be50625f4f484b95001b0ab831aab84f085e
yuipeee <nooof.0827@gmail.com>
2021-05-18 14:57:20 +0900

    投稿完了画面の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,13 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :weight, :text)
+  end
 end
