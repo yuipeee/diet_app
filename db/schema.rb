c65be8b2e195a3cbcb7959b98359e69c0a75a8c7
yuipeee <nooof.0827@gmail.com>
2021-05-19 16:20:16 +0900

    nameカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_19_051919) do
+ActiveRecord::Schema.define(version: 2021_05_19_071741) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "text", null: false
     t.integer "weight", null: false
     t.datetime "created_at", precision: 6, null: false
