37bf4572a593630d7a1337f3a2fedbe8384204d7
yuipeee <nooof.0827@gmail.com>
2021-05-19 14:54:16 +0900

    マイページへのアクセス実装

--- db/migrate/20210518023422_create_posts.rb
+++ db/migrate/20210518023422_create_posts.rb
@@ -1,7 +1,6 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :name,      null: false
       t.string :text,      null: false
       t.integer :weight,   null: false
 
