a3eaf0195b9cad1c2906cb80d492b1b89f9fe08c
yuipeee <nooof.0827@gmail.com>
2021-05-21 14:59:30 +0900

    非同期いいねの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,18 @@
 class LikesController < ApplicationController
+  before_action :post_params
+
   def create
     Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to posts_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    @post = Post.find(params[:id])
+  end
+
 end
