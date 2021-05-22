dbf4c9f972ef8a18707ecad939ad6ba3d46b805e
yuipeee <nooof.0827@gmail.com>
2021-05-22 15:06:00 +0900

    一覧ページの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   with_options presence: true do
   validates :text
-  validates :weight, numericality: { less_than_or_equal_to: 150 },format: { with:/\A[0-9]+\Z/ }
+  validates :weight, numericality: { less_than_or_equal_to: 70 },format: { with:/\A[0-9]+\Z/ }
   end
   
   belongs_to :user
