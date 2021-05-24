67b2bf4998a336ee3b46ccc560bfe5debc21c2e5
yuipeee <nooof.0827@gmail.com>
2021-05-24 11:21:05 +0900

    いいねエラー解消

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show, :search]
+  before_action :authenticate_user!
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
